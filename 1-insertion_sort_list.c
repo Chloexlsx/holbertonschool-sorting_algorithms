c3d6d81edff571a37652e75b0b66c28aba88a20f
Chloexlsx <chloe850350@gmail.com>
2025-08-12 11:49:55 +0000

    task 1

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,56 @@
+#include "sort.h"
+
+/**
+ * swap_with_prev - Swap `node` with its previous node in a doubly linked list
+ * @list: the pointer to the list to sawp
+ * @node: the pointer to the node
+ *
+ * Return: void
+ */
+
+
+static void swap_with_prev(listint_t **list, listint_t *node)
+{
+	listint_t *prev = node->prev;
+
+	/* Detach node from its place */
+	prev->next = node->next;
+	if (node->next)
+		node->next->prev = prev;
+
+	/*move node before prev*/
+	node->prev = prev->prev;
+	node->next = prev;
+
+	if (prev->prev)
+		prev->prev->next = node;
+	else
+		*list = node /*node becomes new head*/
+	prev->prev = node;
+}
+/**
+ * insertion_sort_list - Sort a doubly linked list of ints in ascending order
+ *                       using Insertion sort (by swapping nodes).
+ * @list: Address of the head pointer
+ *
+ * Description: Prints the list after each swap using print_list.
+ * Return: void
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *node, *next;
+
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+		return;
+	for (node = (*list)->next; node; node = next)
+	{
+		/*save next before node possibly moves*/
+		next = node->next;
+		/*swap the current node backward to its correct position*/
+		while (node->prev && node->n < node->prev->n)
+		{
+			sawp_with_prev(list, node);
+			print_list(*list);
+		}
+	}
+}
